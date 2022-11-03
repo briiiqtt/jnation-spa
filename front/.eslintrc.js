b9949466e8ab461ce5471209607ebd2c9ae5e43b
briiiqtt <briiiqtt@gmail.com>
2022-11-03 15:27:21 +0900

    a

---
+++ front/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'plugin:react/recommended',
+  overrides: [],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+  },
+  plugins: ['react'],
+  rules: {},
+};

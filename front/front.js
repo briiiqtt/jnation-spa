bc2748de5dcfacbce8b49edec6c2e411dfb4fe77
briiiqtt <briiiqtt@gmail.com>
2022-11-22 10:30:35 +0900

    라우팅이 문제였네 index가아니라

--- front/front.js
+++ front/front.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get('*', (req, res) => {
   console.log(req.path);
-  res.sendFile(`${__dirname}/build/app.html`);
+  res.sendFile(`${__dirname}/build/index.html`);
 });
 
 app.listen((port = 80), async () => {
